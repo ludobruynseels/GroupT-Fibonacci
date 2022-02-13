23e18e87307964f36efccf70b2c3ae9fa981160d
betelgeuze <ludo.bruynseels@gmail.com>
2022-02-14 00:58:56 +0100

    main veranderd

--- Fibonacci/src/be/kuleuven/groepT/Main.java
+++ Fibonacci/src/be/kuleuven/groepT/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Fibonacci f = new Fibonacci();
-        f.Generate(5);
+        f.Generate(7);
     }
 }
